977b27e5319094d490f64ca8f63c3072edcc1117
Thaw Zin Hein <40501786@live.napier.ac.uk>
2022-03-24 13:49:30 +0630

    create capital class and declare attributes

---
+++ src/main/java/com/napier/gp2/Capital.java
@@ -0,0 +1,16 @@
+package com.napier.gp2;
+
+public class Capital {
+//    name of capital city
+    public String name;
+
+//    country of capital city
+    public String country;
+
+//    district of capital city
+    public String district;
+
+//    population of capital city
+    public int population;
+
+}
