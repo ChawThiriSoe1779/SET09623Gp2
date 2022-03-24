ae657fef378cd35ece9b6ee505c7efcfbafe8a0f
ChawThiriSoe1779 <40501779@live.napier.ac.uk>
2022-03-24 21:02:10 +0630

    updated App.java with codes for getting list of top N populated cities in the world

--- src/main/java/com/napier/gp2/App.java
+++ src/main/java/com/napier/gp2/App.java
@@ -1,6 +1,7 @@
 package com.napier.gp2;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class App
 {
@@ -69,6 +70,46 @@ public class App
         }
     }
 
+    /*
+    Get a list of top N populated cities in the world.
+    @return A list of top N populated cities
+    @var limitno an integer for N in a list of top N populated cities in the world
+     */
+    public ArrayList<City> getTopNPopulatedCity_World()
+    {
+        try
+        {
+            Statement stmt = con.createStatement();  // Create an SQL statement
+            // Create string for SQL statement
+            int limitno = 20;       // for N in a list of Top N populated cities
+            String getCityReports =
+                    "SELECT city.Name, country.Name, city.District, city.Population "
+                            + "FROM city INNER JOIN country on city.CountryCode=country.Code "
+                            + "ORDER BY Population DESC LIMIT " + limitno;
+            // Execute SQL statement
+            ResultSet result = stmt.executeQuery(getCityReports);
+            // Extract city information
+            ArrayList<City> cities = new ArrayList<City>();
+            while (result.next())
+            {
+                City actiy = new City();
+                actiy.name = result.getString("city.Name");
+                actiy.country = result.getString("country.Name");
+                actiy.district = result.getString("city.District");
+                actiy.population = result.getInt("city.Population");
+                cities.add(actiy);
+            }
+            System.out.println("\nList of Top " + limitno + " Populated City in the World\n===========================================================================================");
+            return cities;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get a list of top N populated cities!!");
+            return null;
+        }
+    }
+
 
     public static void main(String[] args)
     {
@@ -78,7 +119,8 @@ public class App
         // Connect to database
         a.connect();
 
-
+        // Extract information of top N populated cities in the world
+        ArrayList<City> cities = a.getTopNPopulatedCity_World();
 
         // Disconnect from database
         a.disconnect();
