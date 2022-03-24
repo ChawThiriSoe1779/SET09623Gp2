77e49821d8216ab36abc4c45c844bb5c3f7d4d65
AungThiha1804 <40501804@live.napier.ac.uk>
2022-03-24 22:49:18 +0630

    Adding countries in the continent from largest population to smallest report function

--- src/main/java/com/napier/gp2/App.java
+++ src/main/java/com/napier/gp2/App.java
@@ -103,7 +103,44 @@ public class App
             return null;
         }
     }
-// Funtion to print countries in the world from largest population to smallest report
+
+    // Funtion to get countries in the Continent from largest population to smallest report data
+    public ArrayList<Country> getCountries_Continent()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            String continent = "'Africa'";
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Code, Name, Continent, Region, Population, Capital FROM country WHERE Continent="+ continent +" ORDER BY Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract countries information
+            ArrayList<Country> countries = new ArrayList<Country>();
+            while (rset.next())
+            {
+                Country coun = new Country();
+                coun.Name = rset.getString("Name");
+                coun.Continent = rset.getString("Continent");
+                coun.Region = rset.getString("Region");
+                coun.Population = rset.getInt("Population");
+                coun.Capital = rset.getString("Capital");
+                countries.add(coun);
+            }
+            System.out.println("\nCountries in the Continent sorted by largest to smallest population\n===========================================================================================");
+            return countries;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country information");
+            return null;
+        }
+    }
+
+// Funtion to print countries report sorted by largest population to smallest
     public void printCountriesReport(ArrayList<Country> countries)
     {
         // Print header
@@ -129,7 +166,9 @@ public class App
         // Countries in the world from largest population to smallest
         ArrayList<Country> countries = a.getCountries_World();
         a.printCountriesReport(countries);
-
+        // Countries in the continent from largest population to smallest
+        countries = a.getCountries_Continent();
+        a.printCountriesReport(countries);
         // Disconnect from database
         a.disconnect();
     }
