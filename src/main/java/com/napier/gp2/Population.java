390ff2368990278282c0b9bd479d7ea7d5d67d99
Thaw Zin Hein <40501786@live.napier.ac.uk>
2022-03-24 13:40:36 +0630

    create population class and declare attributes

---
+++ src/main/java/com/napier/gp2/Population.java
@@ -0,0 +1,17 @@
+package com.napier.gp2;
+
+//Population class with parameter
+public class Population {
+//    name of country, continent or region
+    public String name;
+
+//    total population of country, continent or region
+    public int total_population;
+
+//    total city population of country, continent or region
+    public int city_population;
+
+//    total non_city population of country, continent or region
+    public int non_city_population;
+
+}
