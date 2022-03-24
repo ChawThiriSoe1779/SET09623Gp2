b29b672307522c7c834b2d8f698ddee5e3931121
Thaw Zin Hein <40501786@live.napier.ac.uk>
2022-03-24 11:57:54 +0630

    create city class and declare attributes

---
+++ src/main/java/com/napier/gp2/City.java
@@ -0,0 +1,18 @@
+package com.napier.gp2;
+//A parent class of city with attributes
+
+public class City {
+
+//    city name
+    public String name;
+
+//    country of city
+    public String country;
+
+//    district of city
+    public String district;
+
+//    population of city
+    public int population;
+
+}
