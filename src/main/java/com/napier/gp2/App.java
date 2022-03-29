3d6343d01b6fd9d16cc307e0f44a318864620008
Min-heink <40501789@live.napier.ac.uk>
2022-03-29 15:41:51 +0630

    Function to access all the capital cities in the world sorted by lar to small population

--- src/main/java/com/napier/gp2/App.java
+++ src/main/java/com/napier/gp2/App.java
@@ -69,7 +69,68 @@ public class App
             }
         }
     }
+    /**
+     Function to access all the capital cities in the world sorted by largest to smallest population.
+     @return A list of capital cities in the world sorted by largest to smallest population
 
+     **/
+
+    public ArrayList<Capital>getCapCities_World()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement sql = con.createStatement();
+
+            // Create string for SQL statement
+
+            String getCityReports =
+                    "SELECT city.Name, country.Name, city.District, city.Population FROM city INNER JOIN country on country.capital = city.ID ORDER BY Population DESC;";
+
+            // Execute SQL statement
+            ResultSet result = sql.executeQuery(getCityReports);
+
+            // Extract city data
+            ArrayList<Capital> cap_cities = new ArrayList<Capital>();
+            while (result.next())
+            {
+                Capital ca = new Capital();
+                ca.setName(result.getString("city.Name"));
+                ca.getName();
+                ca.setCountry(result.getString("country.Name"));
+                ca.getCountry();
+                ca.setDistrict(result.getString("city.District"));
+                ca.getDistrict();
+                ca.setPopulation(result.getInt("city.Population"));
+                ca.getPopulation();
+                cap_cities.add(ca);
+            }
+            System.out.println("\nCapital Cities in the world continent sorted by largest to smallest population\n===========================================================================================");
+            return cap_cities;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get capital cities in the world database");
+            return null;
+        }
+    }
+/** Function to Print Capital City
+ * @param cap_cities to print list of capital cities **/
+    public void printCapCityReport(ArrayList<Capital> cap_cities) {
+
+        // Print header
+        System.out.println(String.format("%-25s %-25s %-25s %-25s", "City Name", "Country Name", "District", "Population"));
+        System.out.println("===========================================================================================");
+
+        // Loop over all capital cities in the list
+        for (Capital city : cap_cities) {
+            String city_string =
+                    String.format("%-25s %-25s %-25s %-25s",
+                            city.getName(), city.getCountry(), city.getDistrict(), city.getPopulation());
+            System.out.println(city_string);
+        }
+    }
 
 
     public static void main(String[] args)
@@ -81,7 +142,10 @@ public class App
         // Connect to database
         a.connect();
 
-
+        // get capital city data of the world
+        ArrayList<Capital> cap_cities = a.getCapCities_World();
+        // print city data
+        a.printCapCityReport(cap_cities);
 
         // Disconnect from database
         a.disconnect();
