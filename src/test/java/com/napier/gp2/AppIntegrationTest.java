c6c12cf629d4f1a4512a6dd9b1d3d0693c63b656
Thaw Zin Hein <40501786@live.napier.ac.uk>
2022-04-26 21:00:59 +0630

    updated integrated testing

--- src/test/java/com/napier/gp2/AppIntegrationTest.java
+++ src/test/java/com/napier/gp2/AppIntegrationTest.java
@@ -169,40 +169,138 @@ public class AppIntegrationTest {
     @Test
     void getTotalPopulation() {
         // Extract total population in the world
-        app.getnPrintPopulation_Wrold();
+
+        //expected output of number of people in the world
+        String expectedOutput = "There are 6078749450 people in the world.";
+        //actual output of  number of people in the world
+        String actualOutput = app.getnPrintPopulation_Wrold();
+        assertEquals(expectedOutput, actualOutput);
         System.out.println("Testing to retrieve population of the world successful");
         System.out.println("=================================================================================================");
-
+    }
+    @Test
+    void getTotalPopulationContinent() {
         // Extract total population in the continent
-        String continent = "Africa";
-        app.getnPrintPopulation_Continent(continent);
+        String continent = "Asia";
+        //expected output of number of people in the Asia Continent
+        String expectedOutput = "There are 3705025700 people in the 'Asia' continent.";
+        //actual output of  number of people in the Asia Continent
+        String actualOutput = app.getnPrintPopulation_Continent(continent);
+        assertEquals(expectedOutput, actualOutput);
         System.out.println("Testing to retrieve population of " + continent + " continent successful");
-        System.out.println("=================================================================================================");
-
-        // Extract total population in the region
-        String region = "Southeast Asia";
-        app.getnPrintPopulation_Region(region);
-        System.out.println("Testing to retrieve population of " + region + " caribbean region successful");
-        System.out.println("=================================================================================================");
-
-        // Extract total population in the country
-        String country = "China";
-        app.getnPrintPopulation_Country(country);
-        System.out.println("Testing to retrieve population of " + country + " successful");
-        System.out.println("=================================================================================================");
-
-        // Extract total population in the district
-        String district = "Gelderland";
-        app.getnPrintPopulation_District(district);
-        System.out.println("Testing to retrieve population of " + district + " successful");
-        System.out.println("=================================================================================================");
-
-        // Extract total population in the city
-        String city = "Campina Grande";
-        app.getnPrintPopulation_City(city);
-        System.out.println("Testing to retrieve population of " + city + " successful");
         System.out.println("=================================================================================================");
     }
 
+    @Test
+    void getTotalPopulationRegion() {
+        // Extract total population in the region
+        String region = "Caribbean";
+        //expected output of number of people in the Caribbean Region
+        String expectedOutput = "There are 38140000 people in the 'Caribbean' region.";
+        //actual output of  number of people in the Caribbean Region
+        String actualOutput = app.getnPrintPopulation_Region(region);
+        assertEquals(expectedOutput, actualOutput);
 
+        System.out.println("Testing to retrieve population of " + region + " region successful");
+        System.out.println("=================================================================================================");
+    }
+    @Test
+    void getTotalPopulationCountry() {
+        // Extract total population in the country
+        String country = "Denmark";
+        //expected output of number of people in the Denmark Country
+        String expectedOutput = "There are 5330000 people in the 'Denmark' country.";
+        //actual output of  number of people in the Denmark Country
+        String actualOutput = app.getnPrintPopulation_Country(country);
+        System.out.println("Testing to retrieve population of " + country + " country successful");
+        System.out.println("=================================================================================================");
+    }
+
+    @Test
+    void getTotalPopulationDistrict() {
+        // Extract total population in the district
+        String district = "Gujarat";
+        //expected output of number of people in the Gujarat District
+        String expectedOutput = "There are 8425343 people in the 'Gujarat' district.";
+        //actual output of  number of people in the Gujarat District
+        String actualOutput = app.getnPrintPopulation_Country(district);
+        app.getnPrintPopulation_District(district);
+        System.out.println("Testing to retrieve population of " + district + " district successful");
+        System.out.println("=================================================================================================");
+    }
+
+    @Test
+    void getTotalPopulationCity(){
+        // Extract total population in the city
+        String city = "Seoul";
+       //expected output of number of people in the Seoul city
+        String expectedOutput = "There are 9981619 people in the 'Seoul' city.";
+        //actual output of  number of people in the Seoul City
+        String actualOutput = app.getnPrintPopulation_City(city);
+
+        System.out.println("Testing to retrieve population of " + city + " city successful");
+        System.out.println("=================================================================================================");
+    }
+
+    @Test
+    void getChineseLanguagePopulation() {
+        //get and print population of chinese speakers in the world with percentage
+        System.out.println("Testing to retrieve population of the chinese speaker in the world successful");
+        System.out.println("=================================================================================================");
+
+        //expected output of number of people who speak chinese
+        String expectedOutput = "Number of people who speak Chinese in the world: 1191843539 which is 19.61% of the world population";
+        //actual output of  number of people who speak chinese
+        String actualOutput = app.peopleSpeakPopulation("Chinese");
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void getEnglishLanguagePopulation() {
+        //get and print population of english speakers in the world with percentage
+        System.out.println("Testing to retrieve population of the english speaker in the world successful");
+        //expected output of number of people who speak english
+        String expectedOutput = "Number of people who speak English in the world: 347077867 which is 5.71% of the world population";
+        //actual output of  number of people who speak english
+        String actualOutput = app.peopleSpeakPopulation("English");
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void getHindiLanguagePopulation() {
+
+        //get and print population of Hindi speakers in the world with percentage
+        System.out.println("Testing to retrieve population of the hindi speaker in the world successful");
+        System.out.println("=================================================================================================");
+        //expected output of number of people who speak hindi
+        String expectedOutput = "Number of people who speak Hindi in the world: 405633070 which is 6.67% of the world population";
+        //actual output of  number of people who speak hindi
+        String actualOutput = app.peopleSpeakPopulation("Hindi");
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void getSpanishLanguagePopulation() {
+
+        //get and print population of spanish speakers in the world with percentage
+        System.out.println("Testing to retrieve population of the spanish speaker in the world successful");
+        System.out.println("=================================================================================================");
+        //expected output of number of people who speak spanish
+        String expectedOutput = "Number of people who speak Spanish in the world: 355029462 which is 5.84% of the world population";
+        //actual output of  number of people who speak spanish
+        String actualOutput = app.peopleSpeakPopulation("Spanish");
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void getArabicLanguagePopulation(){
+        //get and print population of arabic population of the arabic speaker in the world successful");
+        System.out.println("=================================================================================================");
+        //expected output of number of people who speak spanish
+        String expectedOutput = "Number of people who speak Arabic in the world: 233839238 which is 3.85% of the world population";
+        //actual output of  number of people who speak spanish
+        String actualOutput = app.peopleSpeakPopulation("Arabic");
+        assertEquals(expectedOutput, actualOutput);
+
+    }
 }
