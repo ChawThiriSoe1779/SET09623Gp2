f17b46acc4e3d66111c4eea8e603fdd3a9a826c0
AungThiha1804 <40501804@live.napier.ac.uk>
2022-04-28 13:02:05 +0630

    fixing error 2

--- src/test/java/com/napier/gp2/AppTest.java
+++ src/test/java/com/napier/gp2/AppTest.java
@@ -480,16 +480,17 @@ public class AppTest
     /**
      * Output a normal language speaker.
      */
-//    @Test
-//    void outputlanguagespeakerReportTest()
-//    {
-//        System.out.println("Language speaker output Testing~~");
-//        String lang_chinese = "testing purpose";
-//        String lang_english = "testing purpose";
-//        String lang_hindi = "testing purpose";
-//        String lang_spanish = "testing purpose";
-//        String lang_arabic = "testing purpose";
-//        app.outputlanguagespeakerReport(lang_chinese,lang_english,lang_hindi,lang_spanish,lang_arabic, "language_speaker_output_testing.md");
-//    }
+    @Test
+    void outputlanguagespeakerReportTest()
+    {
+        System.out.println("Language speaker output Testing~~");
+        ArrayList<Language> langu = new ArrayList<>();
+        Language pop = new Language();
+        pop.setName("Chinese");
+        pop.setPopulation(1191843539);
+        pop.setPercentage(19+'%');
+        langu.add(pop);
+        app.outputlanguagespeakerReport(langu, "language_speaker_output_testing.md");
+    }
 
 }
